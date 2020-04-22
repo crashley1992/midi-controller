ab7a4b2b719df802b91c9f94e895cf4cd237b9a6
Ashley Clarke <crashley92@gmail.com>
2020-04-22 03:41:41 -0700

    added the js for the key events with the piano and made the styling for the piano look a fuck of a lot more like a piano

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # midi-controller
 An app to make beats from your keyboard
+
+pressing down on keyboard events will emitt a drum sound. You can make beats!
+
+Going to add in a record feature and some more instrumentals
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 //event listener for when a key is pressed down
 const keyPress = document.addEventListener("keydown", () => {
-    console.log(event.which + " down");
+    // console.log(event.which + " down");
     keyDrumDown();
 });
 
 //event listener for when a key is released
 const keyReleased = document.addEventListener("keyup", () => {
-    console.log(event.which + " up");
+    // console.log(event.which + " up");
     keyDrumUp();
 });
 
@@ -28,6 +28,49 @@ const keyDrumDown = () => {
         // console.log("f was pressed");
         document.getElementById("f").style.backgroundColor = "#b75d69";
     }   
+    //pianokey events
+    if (event.which === 81) {
+        document.getElementById("q").style.backgroundColor = "#b75d69";
+    } 
+    if (event.which === 50) {
+        document.getElementById("two").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 87) {
+        document.getElementById("w").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 51) {
+        document.getElementById("three").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 69) {
+        document.getElementById("e").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 52) {
+        document.getElementById("four").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 82) {
+        document.getElementById("r").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 53) {
+        document.getElementById("five").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 84) {
+        document.getElementById("t").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 54) {
+        document.getElementById("six").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 89) {
+        document.getElementById("y").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 55) {
+        document.getElementById("seven").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 85) {
+        document.getElementById("u").style.backgroundColor = "#b75d69";
+    }
+    if (event.which === 56) {
+        document.getElementById("eight").style.backgroundColor = "#b75d69";
+    }
 }
 
 //function to links actions to when a key is pressed down. Will controll actions associated with each key relased
@@ -48,5 +91,48 @@ const keyDrumUp = () => {
         console.log("f was pressed");
         document.getElementById("f").style.backgroundColor = "#eacdc2";
     }   
+    //pianokey events
+    if (event.which === 81) {
+        document.getElementById("q").style.backgroundColor = "#eacdc2";
+    } 
+    if (event.which === 50) {
+        document.getElementById("two").style.backgroundColor = "black";
+    }
+    if (event.which === 87) {
+        document.getElementById("w").style.backgroundColor = "#eacdc2";
+    }
+    if (event.which === 51) {
+        document.getElementById("three").style.backgroundColor = "black";
+    }
+    if (event.which === 69) {
+        document.getElementById("e").style.backgroundColor = "#eacdc2";
+    }
+    if (event.which === 52) {
+        document.getElementById("four").style.backgroundColor = "black";
+    }
+    if (event.which === 82) {
+        document.getElementById("r").style.backgroundColor = "#eacdc2";
+    }
+    if (event.which === 53) {
+        document.getElementById("five").style.backgroundColor = "black";
+    }
+    if (event.which === 84) {
+        document.getElementById("t").style.backgroundColor = "#eacdc2";
+    }
+    if (event.which === 54) {
+        document.getElementById("six").style.backgroundColor = "black";
+    }
+    if (event.which === 89) {
+        document.getElementById("y").style.backgroundColor = "#eacdc2";
+    }
+    if (event.which === 55) {
+        document.getElementById("seven").style.backgroundColor = "black";
+    }
+    if (event.which === 85) {
+        document.getElementById("u").style.backgroundColor = "#eacdc2";
+    }
+    if (event.which === 56) {
+        document.getElementById("eight").style.backgroundColor = "black";
+    }
 }
     
